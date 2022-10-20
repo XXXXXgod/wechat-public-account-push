439bd493f77e3da692d39865b1a5f48950caa57e
XXXXXgod <116153439+XXXXXgod@users.noreply.github.com>
2022-10-20 17:48:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,1 +1,36 @@
-{"APP_ID":"wxff86c6b272457047","APP_SECRET":"26b2c70233a410f52056cfd868b1cc38","IS_SHOW_COLOR":true,"CALLBACK_TEMPLATE_ID":"w5U_YCnL7-iI4t-LPIWqZ5888ePB-B8ktB_daTTvGXI","CALLBACK_USERS":[{"name":"自己","id":"oI3sI6EHFrBwouyDa_WF2tL4jjww"}],"USERS":[{"name":"宝贝","id":"oI3sI6A7sZMSonrjjzzXenROcUnA","useTemplateId":"oQ0s528AiIur9e19CSUCfeuXn6P0YnklmKfnjIsHCdU","province":"青海省","city":"西宁市","horoscopeDate":"06-25","horoscopeDateType":"今日","openUrl":"https://XXXXXgod.github.io/","festivals":[{"type":"*生日","name":"宝贝的生日","date":"06-25","year":"1991"},{"type":"*生日","name":"大宝的生日","date":"06-13","year":"2001"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":false,"oneTalk":false,"earthyLoveWords":true,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
+{"APP_ID":"wxff86c6b272457047",
+ "APP_SECRET":"26b2c70233a410f52056cfd868b1cc38",
+   "IS_SHOW_COLOR":true,
+     "CALLBACK_TEMPLATE_ID":"w5U_YCnL7-iI4t-LPIWqZ5888ePB-B8ktB_daTTvGXI",
+       "CALLBACK_USERS":[
+         {"name":"自己",
+          "id":"oI3sI6EHFrBwouyDa_WF2tL4jjww"}
+       ],
+         "USERS":[
+           {"name":"宝贝",
+            "id":"oI3sI6A7sZMSonrjjzzXenROcUnA",
+            "useTemplateId":"oQ0s528AiIur9e19CSUCfeuXn6P0YnklmKfnjIsHCdU",
+            "province":"青海省",
+            "city":"西宁市",
+            "horoscopeDate":"06-25",
+            "horoscopeDateType":"今日",
+            "openUrl":"https://XXXXXgod.github.io/",
+            "festivals":[
+              {"type":"*生日",
+               "name":"宝贝的生日",
+               "date":"06-25",
+               "year":"1991"},
+              {"type":"*生日",
+               "name":"大宝的生日",
+               "date":"06-13",
+               "year":"2001"}],
+            "SWITCH":{"weather":true,
+                      "holidaytts":true,
+                      "CIBA":false,
+                      "oneTalk":false,
+                      "earthyLoveWords":true,
+                      "momentCopyrighting":false,
+                      "poisonChickenSoup":false,
+                      "poetry":false,
+                      "horoscope":false,
+                      "birthdayMessage":true}}
